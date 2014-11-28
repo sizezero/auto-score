2bfe942d5020a486635adefe57c803b40042e92a
Robert Kleemann <robert@kleemann.org>
2014-11-28 15:09:12 -0800

    added top level Framelayout with a couple stacked components

--- src/main/scala/org/kleemann/autoscore/Main.scala
+++ src/main/scala/org/kleemann/autoscore/Main.scala
@@ -3,26 +3,19 @@ package org.kleemann.autoscore
 import org.scaloid.common._
 import android.graphics.Color
 
-import android.view.Menu
-import android.view.MenuItem
+import android.view.{Menu, MenuItem, Gravity}
 
 class Main extends SActivity  {
 
   onCreate {
-    contentView = new SVerticalLayout {
+    contentView = new SFrameLayout {
       style {
         case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
         case t: STextView => t textSize 10.dip
         case e: SEditText => e.backgroundColor(Color.YELLOW)
       }
-      STextView("I am 10 dip tall")
-      STextView("Me too")
-      STextView("I am 15 dip tall") textSize 15.dip // overriding
-      this += new SLinearLayout {
-	STextView("Button: ")
-        SButton(R.string.red)
-      }.wrap
       SEditText("Yellow input field fills the space").fill
+      SButton(R.string.red).<<.Gravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL).>>
     } padding 20.dip
   }
 
